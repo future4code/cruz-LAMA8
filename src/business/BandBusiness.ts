aba32ac4d715a94ca171ab89d2e86c732df047cf
Elber-Sousa <elbe995@gmail.com>
2021-06-25 19:48:39 -0300

    1, 2, 3, 4 finalizadas

---
+++ src/business/BandBusiness.ts
@@ -0,0 +1,47 @@
+import { BandInputDTO, UserRole } from '../model/User';
+import { IdGenerator } from '../services/IdGenerator';
+import { Authenticator } from '../services/Authenticator';
+import { BandDatabase } from '../data/BandDataBase';
+
+export class BandBusiness {
+  async createBand(band: BandInputDTO) {
+    const token = new Authenticator();
+    const resulttoken = token.getData(band.token);
+
+    if (!resulttoken) {
+      throw new Error('Invalid Token');
+    }
+
+    if (resulttoken.role !== UserRole.ADMIN) {
+      throw new Error('You dont have permission to acess this');
+    }
+
+    if (!band.name || !band.music_genre || !band.responsible) {
+      throw new Error('Invalid Arguments');
+    }
+
+    const idGenerator = new IdGenerator();
+    const id = idGenerator.generate();
+
+    const bandDatabase = new BandDatabase();
+    await bandDatabase.createBand(
+      id,
+      band.name,
+      band.music_genre,
+      band.responsible
+    );
+  }
+
+  async getBandbyName(name: string) {
+    if (!name) {
+      throw new Error('Invalid Arguments');
+    }
+
+    const bandDatabase = new BandDatabase();
+    const bandName = await bandDatabase.getBand(name);
+    if (!bandName) {
+      throw new Error('Band not found');
+    }
+    return bandName;
+  }
+}
--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -1,43 +1,59 @@
-import { UserInputDTO, LoginInputDTO } from "../model/User";
-import { UserDatabase } from "../data/UserDatabase";
-import { IdGenerator } from "../services/IdGenerator";
-import { HashManager } from "../services/HashManager";
-import { Authenticator } from "../services/Authenticator";
+import { UserInputDTO, LoginInputDTO } from '../model/User';
+import { UserDatabase } from '../data/UserDatabase';
+import { IdGenerator } from '../services/IdGenerator';
+import { HashManager } from '../services/HashManager';
+import { Authenticator } from '../services/Authenticator';
 
 export class UserBusiness {
-
-    async createUser(user: UserInputDTO) {
-
-        const idGenerator = new IdGenerator();
-        const id = idGenerator.generate();
-
-        const hashManager = new HashManager();
-        const hashPassword = await hashManager.hash(user.password);
-
-        const userDatabase = new UserDatabase();
-        await userDatabase.createUser(id, user.email, user.name, hashPassword, user.role);
-
-        const authenticator = new Authenticator();
-        const accessToken = authenticator.generateToken({ id, role: user.role });
-
-        return accessToken;
+  async createUser(user: UserInputDTO) {
+    if (!user.email || !user.password || !user.role || !user.name) {
+      throw new Error('Invalid Arguments');
     }
 
-    async getUserByEmail(user: LoginInputDTO) {
+    const idGenerator = new IdGenerator();
+    const id = idGenerator.generate();
 
-        const userDatabase = new UserDatabase();
-        const userFromDB = await userDatabase.getUserByEmail(user.email);
+    const hashManager = new HashManager();
+    const hashPassword = await hashManager.hash(user.password);
 
-        const hashManager = new HashManager();
-        const hashCompare = await hashManager.compare(user.password, userFromDB.getPassword());
+    const userDatabase = new UserDatabase();
+    await userDatabase.createUser(
+      id,
+      user.email,
+      user.name,
+      hashPassword,
+      user.role
+    );
 
-        const authenticator = new Authenticator();
-        const accessToken = authenticator.generateToken({ id: userFromDB.getId(), role: userFromDB.getRole() });
+    const authenticator = new Authenticator();
+    const accessToken = authenticator.generateToken({ id, role: user.role });
 
-        if (!hashCompare) {
-            throw new Error("Invalid Password!");
-        }
+    return accessToken;
+  }
 
-        return accessToken;
+  async getUserByEmail(user: LoginInputDTO) {
+    if (!user.email || !user.password || user.email.indexOf('@') === -1) {
+      throw new Error('Invalid Arguments');
     }
+
+    const userDatabase = new UserDatabase();
+    const userFromDB = await userDatabase.getUserByEmail(user.email);
+    const hashManager = new HashManager();
+    const hashCompare = await hashManager.compare(
+      user.password,
+      userFromDB.getPassword()
+    );
+
+    const authenticator = new Authenticator();
+    const accessToken = authenticator.generateToken({
+      id: userFromDB.getId(),
+      role: userFromDB.getRole()
+    });
+
+    if (!hashCompare) {
+      throw new Error('Invalid Password!');
+    }
+
+    return accessToken;
+  }
 }
